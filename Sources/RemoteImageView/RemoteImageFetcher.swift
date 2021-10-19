e4c0780ad2a97b374ddb36be360f005a7acb9760
Esmaeel <rahimie3maeel@gamil.com>
2021-10-19 13:12:44 +0330

    new swifty  initializer

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -33,9 +33,13 @@ public class RemoteImageFetcher: ObservableObject {
   @Published var imageData = Data()
   let url: URL
 
-  public init(url: URL) {
-    self.url = url
-  }
+//  public init(url: URL) {
+//    self.url = url
+//  }
+    
+    public init(from url: URL) {
+        self.url = url
+    }
 
   // 1
   public func fetch() {
